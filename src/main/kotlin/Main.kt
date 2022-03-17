661b4416980e8c33aae66bd1dab86efc6a365210
Aswinivscode <aswinikd333@gmail.com>
2022-03-16 23:41:30 -0700

    task1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,14 @@
-fun sumOfDigits(num: Int) {
+fun main(args: Array<String>) {
+    println(" ")
+    print("Enter the number : ")
+    val num= readLine()!!.toInt()
+    val sum = sumOfDigits(num)
+    println("Sum of the digits : $sum")
+}
+fun sumOfDigits(num: Int) : Int{
     if (num != 0) {
         return num%10 + sumOfDigits(num/10)
-    }
-    fun main() {
-        println("Enter the number : ")
-        var num= readLine()!!.toInt()
-        val sum = sumOfDigits(num)
-        println("Sum of the digits : $sum")
+    }else{
+        return num
     }
 }
